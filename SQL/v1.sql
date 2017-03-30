f2a6abd399d9c2a1ce4a036911b1d8e7646bf5b8
Théo BOUGE <theo.bouge@gmail.com>
2017-03-30 15:19:53 +0200

    Add script to generate the database

---
+++ SQL/v1.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `minified` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `token` varchar(100) NOT NULL,
+  `url` varchar(4000) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
