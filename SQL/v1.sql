1f5aadabfb8003e28bce0b62ae6b7579a16860d6
Theo BOUGE <theo.bouge@gmail.com>
2017-03-31 09:35:52 +0200

    Update database creation script

--- SQL/v1.sql
+++ SQL/v1.sql
@@ -2,6 +2,20 @@ CREATE TABLE `minified` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `token` varchar(100) NOT NULL,
   `url` varchar(4000) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  CONSTRAINT minified_PK PRIMARY KEY (`id`)
+  CONSTRAINT minified_UN UNIQUE KEY (`token`)
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8
+COLLATE=utf8_general_ci ;
 
+CREATE TABLE minifier_db.`user` (
+	id INT UNSIGNED NULL AUTO_INCREMENT,
+	username varchar(100) NOT NULL,
+	hash varchar(60) NOT NULL,
+	CONSTRAINT user_PK PRIMARY KEY (`id`),
+	CONSTRAINT user_UN UNIQUE KEY (`username`)
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8
+COLLATE=utf8_general_ci ;
