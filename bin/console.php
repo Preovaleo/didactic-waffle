2add318f9d3ec597ccffcb18782428a7ab631cf5
Théo BOUGE <theo.bouge@gmail.com>
2017-04-11 00:32:39 +0200

    Add a command to minfiy the js

--- bin/console.php
+++ bin/console.php
@@ -5,9 +5,11 @@ require_once sprintf('%s/../vendor/autoload.php', dirname(__FILE__));
 
 use Symfony\Component\Console\Application;
 use Minifier\Command\CreateUserCommand;
+use Minifier\Command\AssetMinifyCommand;
 
 $app = new Application();
 
 $app->add(new CreateUserCommand());
+$app->add(new AssetMinifyCommand());
 
 $app->run();
--- src/Command/AssetMinifyCommand.php
+++ src/Command/AssetMinifyCommand.php
@@ -1,7 +1,50 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: tbouge
- * Date: 10/04/17
- * Time: 19:06
- */
+
+namespace Minifier\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use MatthiasMullie\Minify;
+
+class AssetMinifyCommand extends Command
+{
+    protected function configure()
+    {
+        $this->setName('asset:minify')
+            ->setDescription('Minify the js and css assets')
+            ->setHelp('Normaly you don\'t have to use this command if your are not a dev');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $root = dirname(dirname(dirname(__FILE__)));
+        $wwwjs = sprintf('%s/www/js', $root);
+        $jsdir = sprintf('%s/js', $root);
+        $jss = $this->searchFileinSubDir($jsdir);
+
+        foreach ($jss as $js) {
+            $output->writeln(sprintf('minify %s to %s', $js[0], $js[1]));
+            $minifier = new Minify\JS($js[0]);
+            $minifier->minify(sprintf('%s/%s', $wwwjs, $js[1]));
+        }
+
+    }
+
+    private function searchFileinSubDir($dir)
+    {
+        $scanned_directory = array_diff(scandir($dir), array('..', '.'));
+        $ret = array();
+
+        foreach ($scanned_directory as $sdir) {
+
+            $tmpdir = sprintf('%s/%s', $dir, $sdir);
+            $dircontent = array_diff(scandir($tmpdir), array('..', '.'));
+
+            foreach ($dircontent as $file) {
+                array_push($ret, array(sprintf('%s/%s', $tmpdir, $file), sprintf('%s_%s', $sdir, $file)));
+            }
+        }
+        return $ret;
+    }
+}
